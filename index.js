b662bb905f0c72e53763af53ae0cc587a8f78702
Parthiv <parthivsaikia985@gmail.com>
2024-03-18 23:50:09 +0530

    completed

--- index.js
+++ index.js
@@ -1,52 +1,114 @@
-let moves = ["ROCK","PAPER","SCISSOR"];
-let playerScore = 0;
-let computerScore = 0;
+function game(){
+    let moves = 0;
+    let playerScore = 0;
+    let computerScore = 0;
+    const infoDiv = document.querySelector(".gameInfo");
+    function playGame(){
+        const rockBtn = document.querySelector(".rock");
+        const paperBtn = document.querySelector(".paper");
+        const scissorBtn = document.querySelector(".scissor");
+        const movesLeft = document.querySelector(".movesLeft");
+        const playerOptions = [rockBtn,paperBtn,scissorBtn];
+        const computerOptions = ["rock","paper","scissor"];
+        playerOptions.forEach(option => {
+            option.addEventListener("click",function(){
+                moves++;
+                movesLeft.textContent = `moves left : ${10-moves}`;
+                const choiceNumber = Math.floor(Math.random()*3);
+                const computerChoice = computerOptions[choiceNumber];
 
-function gameLoop(){
-    for( i = 0;i< 5; i++){
-        let playerSelection = prompt("Chose between rock, paper and scissors");
-        let checkPS = playerSelection.toUpperCase();
-        let computerSelection = moves[Math.floor(Math.random()* 3)];
-        if(checkPS == computerSelection){
-            alert("Draw!");
-        }else if(checkPS == "ROCK" && computerSelection == "PAPER"){
-            alert(`You lose. You chose ${checkPS} and computer chose ${computerSelection}`);
-            playerScore -= 10;
-            computerScore += 10;
-        }else if(checkPS == "ROCK" && computerSelection == "SCISSOR"){
-            alert(`You won. You chose ${checkPS} and computer chose ${computerSelection}`);
-            playerScore+= 10;
-            computerScore-= 10;
-        }else if(checkPS == "PAPER" && computerSelection == "ROCK"){
-            alert(`You won. You chose ${checkPS} and computer chose ${computerSelection}`);
-            playerScore += 10;
-            computerScore -= 10;
-        }else if(checkPS == "PAPER" && computerSelection == "SCISSOR"){
-            alert(`You lose. You chose ${checkPS} and computer chose ${computerSelection}`);
-            playerScore -= 10;
-            computerScore += 10;
-        }else if(checkPS == "SCISSOR" && computerSelection == "PAPER"){
-            alert(`You won. You chose ${checkPS} and computer chose ${computerSelection}`);
-            playerScore += 10;
-            computerScore -= 10;
-        }else if(checkPS == "SCISSOR" && computerSelection == "ROCK"){
-            alert(`You lose. You chose ${checkPS} and computer chose ${computerSelection}`);
-            playerScore -= 10;
-            computerScore += 10;
-        }else{
-            alert("Wrong input");
-            i -= 1;
-        }
+                
+                // infoDiv.textContent = "" 
+
+                winner(this.innerText,computerChoice)
+                if(moves == 10){
+                    gameOver(movesLeft,playerOptions);
+                }
+            })
+        })
+
         
     }
-    alert(`Your score is ${playerScore} and computer's score is ${computerScore}`);
-    playerScore = 0;
-    computerScore = 0;
 
+    function winner(player,computer){
+        
+        const playerScoreBoard = document.querySelector(".pScore");
+        const computerScoreBoard = document.querySelector(".cScore");
 
+        player = player.toLowerCase();
+        computer = computer.toLowerCase();
+        if(player === computer){
+            infoDiv.textContent = "Draw hoise🥱" ;
+            playerScoreBoard.textContent = `Your score is : ${playerScore}`;
+            computerScoreBoard.textContent = `Computer's score is: ${computerScore}`;          
+        }else if(player == "rock"){
+            if(computer == "scissor"){
+                infoDiv.textContent = "Wohooo jikili reward lo😘😘😘😘😘" ;
+                playerScore++;
+                playerScoreBoard.textContent = `Your score is : ${playerScore}`;
+                computerScoreBoard.textContent = `Computer's score is: ${computerScore}`;
+            }else{
+                infoDiv.textContent = "Chii harili 💩💩💩💩💩" ;
+                computerScore++;
+                playerScoreBoard.textContent = `Your score is : ${playerScore}`;
+                computerScoreBoard.textContent = `Computer's score is: ${computerScore}`;
+            }
+        }else if(player == "paper"){
+            if(computer == "scissor"){
+                infoDiv.textContent = "Chii harili 💩💩💩💩💩" ;
+                computerScore++;
+                playerScoreBoard.textContent = `Your score is : ${playerScore}`;
+                computerScoreBoard.textContent = `Computer's score is: ${computerScore}`;
+            }else{
+                infoDiv.textContent = "Wohooo jikili reward lo😘😘😘😘😘" ;
+                playerScore++;
+                playerScoreBoard.textContent = `Your score is : ${playerScore}`;
+                computerScoreBoard.textContent = `Computer's score is: ${computerScore}`;
+            }
+        }else if(player == "scissor"){
+            if(computer == "paper"){
+                infoDiv.textContent = "Wohooo jikili reward lo😘😘😘😘😘" ;
+                playerScore++;
+                playerScoreBoard.textContent = `Your score is : ${playerScore}`;
+                computerScoreBoard.textContent = `Computer's score is: ${computerScore}`;
+            }else{
+                infoDiv.textContent = "Chii harili 💩💩💩💩💩" ;
+                computerScore++;
+                playerScoreBoard.textContent = `Your score is : ${playerScore}`;
+                computerScoreBoard.textContent = `Computer's score is: ${computerScore}`;
+            }
+        }
+    }
+
+    function gameOver(movesLeft,playerOptions){
+        playerOptions.forEach(options => {
+            options.style.display = "none";
+        })
+        movesLeft.style.display = "none";
+        const resultBoard = document.querySelector(".result");
+        infoDiv.style.display = "none";
+        // const reloadbtn = document.querySelector(".reload");
+        
+        if(playerScore == computerScore){
+            resultBoard.textContent = "Jen ten draw koriso ru ";
+        }else if(playerScore > computerScore){
+            resultBoard.textContent = "😘😘😘😘😘 gutei round tu jikili";          
+        }else{
+            resultBoard.textContent = "gutei round tu harili💩💩💩💩💩";
+        }
+        const reloadbtn = document.createElement('button');
+        resultBoard.appendChild(reloadbtn);
+        reloadbtn.textContent = "Ako khel 🥱";
+        reloadbtn.addEventListener('click',()=>{
+            window.location.reload();
+        })
+
+        // reloadbtn.textContent = "Ako khel ";
+        // let lastmsg = document.querySelector(".secondpart")
+        // lastmsg.textContent = "I love you jaan ";
+        // lastmsg.setAttribute('font-size','200px');
+        
+    }
+    playGame();
 }
-
-let btn = document.querySelector(".btn");
-btn.addEventListener("click",gameLoop);
-
-// gameLoop()
+game();
